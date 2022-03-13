16b883fe208f230cb55c049b6f7b985b700282a2
matheusg18 <matheusgabriel.18@gmail.com>
2022-03-13 19:35:46 -0300

    ajusta service/login aos testes

--- src/services/User/login.js
+++ src/services/User/login.js
@@ -1,12 +1,12 @@
 const { User } = require('../../sequelize/models');
+const NotFoundError = require('../../utils/NotFoundError');
 const BadRequestError = require('../../utils/BadRequestError');
 
 module.exports = async ({ email, password }) => {
-  const logged = await User.findOne({ where: { email, password } });
+  const user = await User.findOne({ where: { email } });
 
-  if (!logged) {
-    throw new BadRequestError('Invalid fields');
-  }
+  if (!user) throw new NotFoundError('email not found');
+  if (user.password !== password) throw new BadRequestError('incorrect password');
 
   return true;
 };
